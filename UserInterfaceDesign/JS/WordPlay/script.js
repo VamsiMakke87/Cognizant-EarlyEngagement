f148155fa46dda48e05b9cfe6b7be6f6b69cb187
vamsimakke@gmail.com <vamsimakke@gmail.com>
2022-11-28 22:18:23 +0530

    commit

--- UserInterfaceDesign/JS/WordPlay/script.js
+++ UserInterfaceDesign/JS/WordPlay/script.js
@@ -1,3 +1,25 @@
 function wordPlay(number){
     //fill the code
+    if(number>50)return "Range is High";
+    else if(number<1)return "Not Valid";
+    else{
+        var str="";
+
+        for(i=1;i<=number;i++){
+            var three=Boolean(i%3===0);
+            var five=Boolean(i%5===0);
+
+            if(three && five)
+                str+="Jump ";
+            else if(three)
+                str+="Tap ";
+            else if(five)
+                str+="Clap ";
+            else 
+                str+=i+" ";
+        }
+
+        return " "+str.trim();
+
+    }
 }
