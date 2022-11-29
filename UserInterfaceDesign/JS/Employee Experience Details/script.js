76650848fc191f5e64f934d648dce1aa7789ecb4
vamsimakke@gmail.com <vamsimakke@gmail.com>
2022-11-29 12:08:27 +0530

    commit

---
+++ UserInterfaceDesign/JS/Employee Experience Details/script.js
@@ -0,0 +1,42 @@
+class Employee 
+{
+//fill code here
+
+
+    constructor(name, designation, year_of_experience){
+        this.name=name;
+        this.designation=designation;
+        this.year_of_experience=year_of_experience;
+
+    }
+
+}
+
+function createEmployee(name, designation, year_of_experience)
+{
+//fill code here
+    return new Employee(name, designation, year_of_experience);
+}
+
+function validateObject(employee)
+{
+//fill code here
+    return employee instanceof Employee;
+}
+
+function displayEmployee(name, designation, year_of_experience){
+    var emp=createEmployee(name, designation, year_of_experience);
+    var str="";
+    if(validateObject(emp)){
+        var exp=new Date().getFullYear()-emp.year_of_experience;
+        str=emp.name+" is serving the position of "+emp.designation+" since "+exp;
+    }
+
+    // console.log(emp.name);
+    return str;
+
+
+}
+
+
+displayEmployee("Jerold","Manager",15);
